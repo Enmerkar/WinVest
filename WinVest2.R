8ba9a05a82d0dbb38aecf5131f04dfc77c1db885
Justin Knife <jkni3047@uni.sydney.edu.au>
2016-03-23 17:47:27 +1100

    WinVest3 created.

--- WinVest2.R
+++ WinVest2.R
@@ -17,6 +17,7 @@ run_encrisp <- function(corr) {
   pattern.a[length(pattern.a)]=0
   pattern.a
 }
+
 # removes correlated signs
 shed <- function(x) {
   lag <- c(NA,x[-length(x)])
@@ -33,10 +34,32 @@ run_shed <- function(crisp) {
   pattern.a[2:(length(pattern.a)-1)]
 }
 
-# output
-patterns <- data.frame('exchange.stock', 0.5, 0.5, 5, 0)
-colnames(patterns) <- c('stock', 'correlation', 'amplitude', 'period', 'buy')
+# creates factor labels for correlated regions
+set_regions <- function (crisp) {
+  l <- length(crisp)
+  m <- 1
+  n <- 1
+  lag <- c(NA,crisp[-l])
+  region <- vector(length=l)
+  while (n <= l) {
+    if (n>1 && crisp[n]==(-1*lag[n])) {m <- m+1}
+    if (abs(crisp[n]) != 1) {
+      region[n] = 0
+    } else {
+      region[n] = m
+    }
+    n <- n+1
+  }
+  as.factor(region)
+}
 
+# output
+patterns <- data.frame('exchange.stock', date(), 0.5, 5, 0.5, 5, 0)
+colnames(patterns) <- c('stock', 'date', 'correlation', 'swings', 'amplitude', 'period', 'buy')
+
+# Eventually write this function with polynomial and period arguments
+# Then call iteratively from another function with a polynomial limit argument
+# and a vector of periods argument
 analyseStock <- function (stock) {
   
   stock='qan'
@@ -158,6 +181,12 @@ analyseStock <- function (stock) {
   data.12$corr.3 <- ifelse(sign(data.12$resid.3)==sign(data.12$resid.3.lag),sign(data.12$resid.3),0)
   data.12$corr.4 <- ifelse(sign(data.12$resid.4)==sign(data.12$resid.4.lag),sign(data.12$resid.4),0)
   
+  # Overall correlation coefficents
+  corr.3.1 <- sum(abs(data.3$corr.1), na.rm=TRUE)/(nrow(data.3)-1)
+  corr.3.2 <- sum(abs(data.3$corr.2), na.rm=TRUE)/(nrow(data.3)-1)
+  corr.3.3 <- sum(abs(data.3$corr.3), na.rm=TRUE)/(nrow(data.3)-1)
+  corr.3.4 <- sum(abs(data.3$corr.4), na.rm=TRUE)/(nrow(data.3)-1)
+  
   # Clean correlations until they are smooth and stable
   data.3$crisp.1 <- run_encrisp(data.3$corr.1)
   data.3$crisp.2 <- run_encrisp(data.3$corr.2)
@@ -179,6 +208,18 @@ analyseStock <- function (stock) {
   data.12$crisp.3 <- run_encrisp(data.12$corr.3)
   data.12$crisp.4 <- run_encrisp(data.12$corr.4)
   
+  # Need to give each region a factor label
+  data.3$region.1 <- set_regions(data.3$crisp.1)
+  data.3$region.2 <- set_regions(data.3$crisp.2)
+  data.3$region.3 <- set_regions(data.3$crisp.3)
+  data.3$region.4 <- set_regions(data.3$crisp.4)
+  
+  # average period length of regions
+  period.3.1 <- mean(as.vector(table(data.3$region.1))[-1])
+  period.3.2 <- mean(as.vector(table(data.3$region.2))[-1])
+  period.3.3 <- mean(as.vector(table(data.3$region.3))[-1])
+  period.3.4 <- mean(as.vector(table(data.3$region.4))[-1])
+  
   # Get max/min extrema according to correlations
   data.3$extrema.1 <- ifelse(data.3$crisp.1==1,data.3$high-data.3$model.1,ifelse(data.3$crisp.1==(-1),data.3$low-data.3$model.1,0))
   data.3$extrema.2 <- ifelse(data.3$crisp.2==1,data.3$high-data.3$model.2,ifelse(data.3$crisp.2==(-1),data.3$low-data.3$model.2,0))
@@ -191,50 +232,46 @@ analyseStock <- function (stock) {
   pattern.3.3 <- run_shed(data.3$crisp.3)
   pattern.3.4 <- run_shed(data.3$crisp.4)
   
-  # Need to give each region a factor label
-  # Then extract max/min extrema from each region
-  # I only need to pass over each vector once
-  region <- function (pattern, crisp) {
-    l <- length(crisp)
-    m <- 1
-    n <- 1
-    lag <- c(NA,crisp[-l])
-    region <- vector(length=l)
-    while (n <= l) {
-      if (n>1 && crisp[n]==(-1*lag[n])) {m <- m+1}
-      if (abs(crisp[n]) != 1) {
-        region[n] = 0
-      } else {
-        region[n] = m
-      }
-      n <- n+1
-    }
-    as.factor(region)
-  }
+  # count swings
+  swings.3.1 <- length(pattern.3.1)
+  swings.3.2 <- length(pattern.3.2)
+  swings.3.3 <- length(pattern.3.3)
+  swings.3.4 <- length(pattern.3.4)
   
-  # Overall correlation coefficents
-  corr.3.1 <- sum(abs(data.3$corr.1), na.rm=TRUE)/(nrow(data.3)-1)
-  corr.3.2 <- sum(abs(data.3$corr.2), na.rm=TRUE)/(nrow(data.3)-1)
-  corr.3.3 <- sum(abs(data.3$corr.3), na.rm=TRUE)/(nrow(data.3)-1)
-  corr.3.4 <- sum(abs(data.3$corr.4), na.rm=TRUE)/(nrow(data.3)-1)
+  # replicate region factors for patterns
+  region.3.1 <- as.factor(seq(1:length(pattern.3.1)))
+  region.3.2 <- as.factor(seq(1:length(pattern.3.2)))
+  region.3.3 <- as.factor(seq(1:length(pattern.3.3)))
+  region.3.4 <- as.factor(seq(1:length(pattern.3.4)))
+  
+  # Then extract max/min extrema from each region
+  extrema.3.1 <- as.vector(aggregate(abs(data.3$extrema.1), by=list(data.3$region.1), FUN=max))[-1,2]*pattern.3.1
+  extrema.3.2 <- as.vector(aggregate(abs(data.3$extrema.2), by=list(data.3$region.2), FUN=max))[-1,2]*pattern.3.2
+  extrema.3.3 <- as.vector(aggregate(abs(data.3$extrema.3), by=list(data.3$region.3), FUN=max))[-1,2]*pattern.3.3
+  extrema.3.4 <- as.vector(aggregate(abs(data.3$extrema.4), by=list(data.3$region.4), FUN=max))[-1,2]*pattern.3.4
   
   # Overall amplitude coefficients
+  amplitude.3.1 <- mean(abs(diff(extrema.3.1, lag=1, differences=1)))
+  amplitude.3.2 <- mean(abs(diff(extrema.3.2, lag=1, differences=1)))
+  amplitude.3.3 <- mean(abs(diff(extrema.3.3, lag=1, differences=1)))
+  amplitude.3.4 <- mean(abs(diff(extrema.3.4, lag=1, differences=1)))
   
-  
-  # Buy Index: measure of closeness to minimum (1 to -1) of cycle.
-  
+  # Buy Index: measure of closeness to minimum of cycle.
+  # 1 means current price is at trough of cycle - ideal time to buy
+  # -1 means current price is at peak of cycle - ideal time to sell
+  buy.3.1 <- data.3$resid.1[length(data.3$resid.1)]/(amplitude.3.1/-2)
+  buy.3.2 <- data.3$resid.1[length(data.3$resid.2)]/(amplitude.3.2/-2)
+  buy.3.3 <- data.3$resid.1[length(data.3$resid.3)]/(amplitude.3.3/-2)
+  buy.3.4 <- data.3$resid.1[length(data.3$resid.4)]/(amplitude.3.4/-2)
   
   # choose only patterns which have a minimum number of appropriate oscillations
   # e.g. at least 2 swings but no more than 10.
   
-  
-  # return good patterns
-  #if(corr3>0.5 && vol3>0.5) patterns <- rbind(patterns, c(stock, corr3, vol3, per3, buy))
-  #if(corr5>0.5 && vol5>0.5) patterns <- rbind(patterns, c(stock, corr5, vol5, per5, buy))
-    
+  # Return aggregate information
+  patterns <- rbind(patterns, c(stock, date(), corr.3.1, swings.3.1, amplitude.3.1, period.3.1, buy.3.1))
   
   # clean environment
-  #rm(data)
+  rm(data)
   
 }
 
